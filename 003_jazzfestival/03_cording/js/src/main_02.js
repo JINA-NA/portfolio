13d20dbc189bccf0a1ecaf0a8c10a4cab3b11d69
njina <najina603@gmail.com>
2020-05-25 18:00:21 +0900

    laptop 반응형 얼추 완료. tablet css 얼추 완성(navigation 해야함)

--- 003_jazzfestival/03_cording/js/src/main_02.js
+++ 003_jazzfestival/03_cording/js/src/main_02.js
@@ -9,11 +9,14 @@ var wrap = $('#wrap');
 var headBox =  $('#headBox');
 var viewBox =  $('#viewBox');
 var ticketBox = $('#ticketBox');
+var timetableBox = $('#timetableBox');
+var locationBox = $('#locationBox');
 
 var headBoxW = headBox.innerWidth();
+var win = $(window);
 var winHeight;
 var WinHeightSet = function(){
-	winHeight = $(window).height();
+	winHeight = win.height();
 	headBox.innerHeight( winHeight );
 	viewBox.innerHeight( winHeight );
 };
@@ -21,7 +24,7 @@ WinHeightSet();
 
 var wrapOffsetL;
 
-$(window).on('resize',function(){
+win.on('resize',function(){
 	//세로 길이 꽉차게
 	WinHeightSet();
 
@@ -31,25 +34,56 @@ $(window).on('resize',function(){
 });
 
 var headBoxBg = [];
-headBoxBg[0] = 'rgba(0, 127, 118, 1)';
-headBoxBg[1] = 'rgba(255,255,255,0.3)';
-console.log(headBoxBg[0]);
+headBoxBg[0] = 'rgba(30, 100, 80, 1)';
+headBoxBg[1] = 'rgba(30, 100, 80, 0)';
+// console.log(headBoxBg[0]);
 
 
 //스크롤시, headBox 변화
 var myScroll;
-$(window).on('scroll',function(e){
+var timetableBoxList = timetableBox.find('li');
+var locationBoxList = locationBox.find('li');
+win.on('scroll',function(e){
 	e.preventDefault();
-	myScroll = $(window).scrollTop();
-  console.log(myScroll, winHeight);
-	if( myScroll <  winHeight ){
-		console.log('!!!!!');
-		headBox.css({'backgroundColor':headBoxBg[0]});
+	myScroll = win.scrollTop();
+  // console.log(myScroll, winHeight);
+	
+	//헤드박스 배경색상 변경
+	if( myScroll+300 <  winHeight ){
+		headBox.stop().animate({'backgroundColor':headBoxBg[0]});
 	} else {
-		console.log('????');
-		headBox.css({'backgroundColor':headBoxBg[1]});
+		headBox.stop().animate({'backgroundColor':headBoxBg[1]});
+	}
+
+	if( myScroll+500 > timetableBox.offset().top ){
+		timetableBoxList.css({position:'relative'});
+		timetableBoxList.eq(0).animate({top:'-100px'},600);
+		timetableBoxList.eq(1).delay(100).animate({top:'-100px'},600);
+	}
+
+	if( myScroll+600 > locationBox.offset().top ){
+		locationBoxList.css({position:'relative'});
+		locationBoxList.eq(0).animate({top:'-50px'},700);
+		locationBoxList.eq(1).delay(200).animate({top:'-50px'},600);
+		locationBoxList.eq(2).delay(400).animate({top:'-50px'},600);
 	}
 });
+
+
+
+
+
+
+//탑버튼 클릭시 최상단으로
+var topBtn = $('.topBtn');
+topBtn.children('button').on('click',function(e){
+	e.preventDefault();
+	$('html,body').scrollTop(0);
+});
+
+
+
+
 
 
 
