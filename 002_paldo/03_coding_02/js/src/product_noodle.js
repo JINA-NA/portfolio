1168b9e4b1bc12ea1d216baafcf07bbb4b14b0a1
njina <najina603@gmail.com>
2020-06-18 18:05:24 +0900

    제품 소개 리스트 작업중

--- 002_paldo/03_coding_02/js/src/product_noodle.js
+++ 002_paldo/03_coding_02/js/src/product_noodle.js
@@ -9,7 +9,7 @@
 
 var pageLink = $('#pageLink');
 pageLink.find('option')
-속성명도 셀렉티드, 속성값도 셀렉티드
+
 
 
 //다른 페이지로 이동-select 태그
@@ -18,6 +18,34 @@ pageLink.on('change',function(){
 	console.log(v);
 	window.location = v;
 });
+
+
+
+// 제품 리스트 ===============================
+var contBox = $('#contBox');
+var proUl = contBox.find('ul');
+var proArr =  [
+	{name:'비빔면', img:'./bibimmen_con_039.png'},
+	{name:'비빔면 매운맛', img:'./bibimmen_con_039.png'},
+	{name:'왕뚜껑', img:'./bibimmen_con_039.png'},
+	{name:'남자라면', img:'./bibimmen_con_039.png'},
+	{name:'일품 해물라면', img:'./bibimmen_con_039.png'},
+	{name:'일품 짜장면', img:'./bibimmen_con_039.png'},
+	{name:'팔도 짜장면', img:'./bibimmen_con_039.png'}
+];
+var proLi;
+for(var i = 0; i < proArr.length; i++){
+	proUl.append('<li><div class="product_img"></div> <p class="product_name">' + proArr[i].name +'</p></li>');
+	proLi = proUl.children('li');
+	
+}
+
+
+
+
+
+
+
 
 
 
