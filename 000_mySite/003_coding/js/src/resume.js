d1fefedbb2521e19f1aa3d6389cb94f19b1d4f6b
njina <najina603@gmail.com>
2020-07-07 18:04:33 +0900

    canvas 그래프 작업중

--- 000_mySite/003_coding/js/src/resume.js
+++ 000_mySite/003_coding/js/src/resume.js
@@ -20,11 +20,13 @@ for(var i = 0 ; i < infoDiv.length ; i++){
 	console.log(infoDivOff)
 }
 
+infoDiv.eq(0).find('span').addClass('active');
+
 win.on('scroll',function(e){
 	myScroll = win.scrollTop();
 	
 	for(var i = 0 ; i < infoDiv.length ; i++){
-		if( myScroll+500 >= infoDivOff[i] ){
+		if( myScroll+700 >= infoDivOff[i] ){
 			infoDiv.eq(i).find('span').addClass('active');
 		}
 	}
@@ -44,17 +46,50 @@ ctx.font = 'bold 1.5rem sans-serif';
 ctx.textAlign = 'center';
 
 //본격적으로 도형 만들기
+//html
 ctx.beginPath();
-
 ctx.arc(75, 75, 60, Math.PI*2, false);
-ctx.fillText('HTML',75,75+10);
+ctx.fillText('HTML', 75, 75+10);
+ctx.stroke();
 
+//css
+ctx.beginPath();
+ctx.arc(250, 75, 60, Math.PI*2, false);
+ctx.fillText('CSS', 250, 75+10);
+ctx.stroke();
 
+//jQuery
+ctx.beginPath();
+ctx.arc(425, 75, 60, Math.PI*2, false);
+ctx.fillText('jQuery', 425, 75+10);
+ctx.stroke();
 
+//sass
+ctx.beginPath();
+ctx.arc(600, 75, 60, Math.PI*2, false);
+ctx.fillText('sass', 600, 75+10);
 ctx.stroke();
 
 
+ctx.font = 'bold 1rem sans-serif';
 
+//illust
+ctx.beginPath();
+ctx.arc(75, 250, 60, Math.PI*2, false);
+ctx.fillText('illust', 75, 250+5);
+ctx.stroke();
+
+//photoshop
+ctx.beginPath();
+ctx.arc(250, 250, 60, Math.PI*2, false);
+ctx.fillText('photoshop', 250, 250+5);
+ctx.stroke();
+
+//indesign
+ctx.beginPath();
+ctx.arc(425, 250, 60, Math.PI*2, false);
+ctx.fillText('indesign', 425, 250+5);
+ctx.stroke();
 
 
 
